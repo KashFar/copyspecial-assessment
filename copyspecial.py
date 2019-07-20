0fdfce3466c7e81738f371e8b9815be112d0bbca
KashF <Kashfarhadi@gmail.com>
2019-07-19 21:33:56 -0400

    printsfilenames

--- copyspecial.py
+++ copyspecial.py
@@ -24,7 +24,9 @@ __author__ = "Kash Farhadi"
 
 def get_special_paths(dir):
     """returns a list of the absolute paths of the special files in the given directory"""
-    pass
+    output = [filename for filename in os.listdir(dir) if re.search(
+        '__(.[A-Za-z]*)__', filename)]
+    return output
 
 def copy_to(paths, dir):
     """ Given a list of paths, copies those files into the given directory"""
@@ -37,22 +39,21 @@ def zip_to(paths, zippath):
 
 
 def main():
-    # This snippet will help you get started with the argparse module.
+    """ Copies, compresses, or prints special files"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('from_dir', help="source dir for copy")
     args = parser.parse_args()
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
+    special_paths=get_special_paths(args.from_dir)
 
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
+    if not args.todir and not args.tozip:
+        for filename in special_paths:
+            print(filename)
 
-    # +++your code here+++
-    # Call your functions
+    
 
 
 if __name__ == "__main__":
